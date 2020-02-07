90dd2420b84b69b4bf46b2f4e5698e4806eafdb8
Ali <merenguy12@gmail.com>
2020-02-07 09:58:06 +0200

    Button

--- Button/Button/button.c
+++ Button/Button/button.c
@@ -2,21 +2,7 @@
 
 STD_levelType Button_Read(uint8 Button_Num)
  {
-   STD_levelType Button_Level=0;
-	switch(Button_Num)
-	{
-	  case  Button0:
-	  Button_Level=Dio_ReadChannel(Button0);
-	  break;
-	
-	  case Button1:	
-	  Button_Level=Dio_ReadChannel(Button1);
-	  break;
-	  
-	  case Button2:
-	  Button_Level=Dio_ReadChannel(Button2);
-	  break;
-    }
-
+     STD_levelType Button_Level=0;
+	 Button_Level=Dio_ReadChannel(Button_Num);
      return Button_Level;
   }
